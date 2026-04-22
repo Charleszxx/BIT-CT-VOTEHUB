d14c392d9f134c08a761ddfd0bdf10ba39203895
Charleszxx <108706480+Charleszxx@users.noreply.github.com>
2026-04-22 12:07:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.static("public"));
 
 const otpStore = {};
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString("utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
