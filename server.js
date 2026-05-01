a868a04a1436d3d282569653cbda18a9e03fa96f
Charleszxx <108706480+Charleszxx@users.noreply.github.com>
2026-05-01 09:46:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const cors = require("cors");
 const QRCode = require("qrcode");
 const admin = require("firebase-admin");
 
-require("dotenv").config();
-
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
