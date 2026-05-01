75f76cf671b14921cc979c411d996aff660542ef
Charleszxx <108706480+Charleszxx@users.noreply.github.com>
2026-05-01 09:21:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4, // 👈 THIS is the missing key fix
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
