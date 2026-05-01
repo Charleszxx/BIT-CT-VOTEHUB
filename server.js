9effedabcd7bf8ca56231bf4dac24c375587870d
Charleszxx <108706480+Charleszxx@users.noreply.github.com>
2026-05-01 09:12:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const db = admin.database();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
